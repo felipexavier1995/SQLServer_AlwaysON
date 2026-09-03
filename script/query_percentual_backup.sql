3ce52bbf21816d5e33f4a6d955b43b174f99cdf0
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2026-09-03 20:44:06 -0300

    Update query_percentual_backup.sql

--- script/query_percentual_backup.sql
+++ script/query_percentual_backup.sql
@@ -7,3 +7,21 @@ getdate()) as estimated_completion_time
 FROM sys.dm_exec_requests r 
 CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) a
 WHERE r.command in ('BACKUP DATABASE','RESTORE DATABASE')
+
+
+
+-- Depois de aplicar tunning da query
+
+SELECT 
+    r.session_id AS SPID,
+    r.command,
+    t.text AS Query,
+    r.start_time,
+    r.percent_complete,
+    DATEADD(SECOND, r.estimated_completion_time / 1000, GETDATE()) AS estimated_completion_time
+FROM (
+    SELECT * 
+    FROM sys.dm_exec_requests
+    WHERE command IN ('BACKUP DATABASE', 'RESTORE DATABASE')
+) r
+CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) t;
