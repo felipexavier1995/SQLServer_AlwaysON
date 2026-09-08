39e730f2e756694e6a05e65cf4db042930209f4b
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2026-09-08 10:40:07 -0300

    Update query_backup.sql

--- script/query_backup.sql
+++ script/query_backup.sql
@@ -1,7 +1,8 @@
 --- query para backup full.
-use [nome do banco de dados]
-
+use [nome do banco de dados];
+go
 
 
 --- query para backup log.
-use [nome do banco de dados]
+use [nome do banco de dados];
+go
